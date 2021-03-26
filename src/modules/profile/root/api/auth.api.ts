4aed52a13ac82d4fd52df300a3b48fbef23dcb39
sergeydevhub <work.sergey.dev@gmail.com>
2021-03-26 17:43:36 +0200

    fix: logout api type fix

--- src/modules/profile/root/api/auth.api.ts
+++ src/modules/profile/root/api/auth.api.ts
@@ -4,4 +4,4 @@ import { TResources, TEndpoint } from "@core/transport";
 import { TriggeredAction } from "@core/helpers/redux/actions";
 
 export const login: TEndpoint = (action: TriggeredAction<LoginDTO>, resource: TResources = 'auth') => `${resource}/login`;
-export const logout: TEndpoint = (action: TriggeredAction<LoginDTO>, resource: TResources = 'auth') => `${resource}/logout`;
+export const logout: TEndpoint = (action: TriggeredAction<LogoutDTO>, resource: TResources = 'auth') => `${resource}/logout`;
