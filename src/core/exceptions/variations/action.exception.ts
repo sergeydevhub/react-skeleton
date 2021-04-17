634d29748d9692f70c716a6fe2bc024a86c4779e
sergeydevhub <work.sergey.dev@gmail.com>
2021-04-17 20:18:29 +0300

    ref: extend action exception available messages

--- src/core/exceptions/variations/action.exception.ts
+++ src/core/exceptions/variations/action.exception.ts
@@ -1,11 +1,17 @@
 import {RootException} from "./root.exception";
-import { ErrorsTranslation } from '@core/localization/locales';
+import {ErrorsTranslation, ValidatorTranslation} from '@core/localization/locales';
+
+type TAvailableMessages = keyof ErrorsTranslation | keyof ValidatorTranslation;
 
 export class ActionException extends RootException {
+  public readonly payload: any;
+
   constructor(
     public readonly type: string,
-    message: keyof ErrorsTranslation = 'common'
+    message: TAvailableMessages = 'common',
+    payload?: any
   ) {
     super(`errors.${message}`);
+    this.payload = payload;
   }
 }
