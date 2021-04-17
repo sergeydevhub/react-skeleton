6c19d3161b7aa17b055955af38c07e7ad2ed0fbe
sergeydevhub <work.sergey.dev@gmail.com>
2021-04-17 15:06:15 +0300

    ref: update localization typings & reducer

--- src/modules/localization/root/localization.module.ts
+++ src/modules/localization/root/localization.module.ts
@@ -1,5 +1,5 @@
 import { Reducer }  from "redux";
-import { ObjectRepository } from "@core/helpers/redux/state";
+import { ObjectRepositoryHelper } from "@core/helpers/redux/state";
 import { ReduxModuleHelper } from "@core/helpers/redux";
 import { getPredefinedLanguage } from "@core/utils";
 import { LocalizationEntity } from "./data";
@@ -10,14 +10,14 @@ export type TState = LocalizationEntity;
 const lang = getPredefinedLanguage();
 export const initialState: TState = new LocalizationEntity({ locale: LOCALES[lang], language: lang });
 
-const localizationModule = new ReduxModuleHelper<TState>('localization', ObjectRepository);
+const localizationModule = new ReduxModuleHelper<TState>('localization', ObjectRepositoryHelper);
 
 const switchLang = localizationModule.sync<TAvailableLangs>(['switch']);
 
 export const reducer = localizationModule.reducer(
-  (repository: ObjectRepository<TState>) => ({
+  (repository: ObjectRepositoryHelper<TState>) => ({
   [switchLang.type]: (
-    state: TState = {...initialState},
+    state: TState = { ...initialState },
     action: ReturnType<typeof switchLang>
   ) => repository.update({ language: action.payload, locale: LOCALES[action.payload] })
 }), { ...initialState });
